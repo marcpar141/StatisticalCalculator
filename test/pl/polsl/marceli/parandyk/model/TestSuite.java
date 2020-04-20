d57e6fc92986342a232fec766485abba78171ffb
pawelsa <pawel.sapek97@gmail.com>
2020-04-20 23:34:44 +0200

    tests written

---
+++ test/pl/polsl/marceli/parandyk/model/TestSuite.java
@@ -0,0 +1,12 @@
+package pl.polsl.marceli.parandyk.model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        ModelTest.class
+})
+public class TestSuite {
+}
